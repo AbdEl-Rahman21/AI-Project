6051dc46be29af3edc195709d27a25b319023418
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-12-08 00:07:05 +0200

    Add input validation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pickle as pkl
 import tkinter as tk
 
@@ -8,6 +10,15 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from tkinter import Button, ttk
 from tkinter import messagebox
+
+df = pd.read_csv('crime_prediction_in_chicago_dataset.csv',
+                 usecols=['Primary Type', 'Location Description']).dropna()
+
+MODEL_TYPE_VALUES = ['Logistic Regression', 'K-Nearest Neighbors', 'Decision Tree', 'Support Vector Machine']
+
+PRIMARY_TYPE_VALUES = df['Primary Type'].unique().tolist()
+
+LOCATION_DESCRIPTION_VALUES = df['Location Description'].unique().tolist()
 
 with open('./saved_models/decision_tree_classifier.pkl', 'rb') as file:
   decision_tree_classifier = pkl.load(file)
@@ -24,28 +35,41 @@ with open('./saved_models/svm_classifier.pkl', 'rb') as file:
 with open('./saved_models/encoder.pkl', 'rb') as file:
   encoder = pkl.load(file)
 
-def validate_number():
-  try:
-    number = int(community_area_box.get())
+def validate_input(primary_type, location_description, community_area, model_type):
+  if primary_type not in PRIMARY_TYPE_VALUES:
+    messagebox.showerror('Invalid Input!', 'Please select a valid Primary Type.')
 
-    if number < 1 or number > 77:
-      messagebox.showerror('Invalid Input!', 'Please enter a number between 1 and 77.')
+    return False
+  elif location_description not in LOCATION_DESCRIPTION_VALUES:
+    messagebox.showerror('Invalid Input!', 'Please select a valid Location Description.')
 
-      community_area_box.delete(0, tk.END)
-      community_area_box.insert(0, '0')
-  except ValueError:
-    messagebox.showerror('Invalid Input!', 'Please enter a valid number.')
+    return False
+  elif model_type not in MODEL_TYPE_VALUES:
+    messagebox.showerror('Invalid Input!', 'Please select a valid Model Type.')
 
-    community_area_box.delete(0, tk.END) # Clear the invalid input
-    community_area_box.insert(0, '0')
+    return False
+  else:
+    try:
+      community_area = int(community_area)
+
+      if community_area < 1 or community_area > 77:
+        messagebox.showerror('Invalid Input!', 'Please select a valid Community Area (from 1 to 77).')
+
+        return False
+    except ValueError:
+      messagebox.showerror('Invalid Input!', 'Please select a valid Community Area (from 1 to 77).')
+
+      return False
+
+  return True
 
 def show_prediction():
-  validate_number
-
   primary_type = primary_type_box.get()
   location_description = location_description_box.get()
-  community_area = int(community_area_box.get())
+  community_area = community_area_box.get()
   model_type = model_type_box.get()
+
+  if not validate_input(primary_type, location_description, community_area, model_type): return
 
   prediction_input = [primary_type, location_description, community_area]
 
@@ -64,7 +88,6 @@ def show_prediction():
       prediction_text.insert(tk.END, f'{svm_classifier.predict(prediction_input)}\n')
       prediction_text.insert(tk.END, f'{svm_classifier.predict_prob(prediction_input)}')
 
-
 root = tk.Tk()
 root.geometry('600x600')
 root.title('Crime Rate in Chicago')
@@ -75,7 +98,7 @@ primary_type_frame.pack(pady=10, padx=10, fill='x')
 primary_type_label = tk.Label(primary_type_frame, text='Primary Type:', font=('Arial', 14))
 primary_type_label.pack(side=tk.LEFT, pady=(10, 5), anchor='w')
 
-primary_type_box = ttk.Combobox(primary_type_frame, values=["Option 1", "Option 2", "Option 3"], font=('Arial', 12), width=20)
+primary_type_box = ttk.Combobox(primary_type_frame, values=PRIMARY_TYPE_VALUES, font=('Arial', 12), width=30)
 primary_type_box.pack(side=tk.LEFT, pady=5, padx=10)
 
 location_description_frame = tk.Frame(root)
@@ -84,7 +107,7 @@ location_description_frame.pack(pady=10, padx=10, fill='x')
 location_description_label = tk.Label(location_description_frame, text='Location Description:', font=('Arial', 14))
 location_description_label.pack(side=tk.LEFT, pady=(10, 5), anchor='w')
 
-location_description_box = ttk.Combobox(location_description_frame, values=["Option 1", "Option 2", "Option 3"], font=('Arial', 12), width=20)
+location_description_box = ttk.Combobox(location_description_frame, values=LOCATION_DESCRIPTION_VALUES, font=('Arial', 12), width=40)
 location_description_box.pack(side=tk.LEFT, pady=5, padx=10)
 
 community_area_frame = tk.Frame(root)
@@ -93,7 +116,7 @@ community_area_frame.pack(pady=10, padx=10, fill='x')
 community_area_label = tk.Label(community_area_frame, text='Community Area:', font=('Arial', 14))
 community_area_label.pack(side=tk.LEFT, padx=(0, 2))
 
-community_area_box = tk.Spinbox(community_area_frame, from_=1, to=77, font=('Arial', 12), validate='key')
+community_area_box = tk.Spinbox(community_area_frame, from_=1, to=77, font=('Arial', 12))
 community_area_box.pack(side=tk.LEFT, pady=10)
 
 model_type_frame = tk.Frame(root)
@@ -102,7 +125,7 @@ model_type_frame.pack(pady=10, padx=10, fill='x')
 model_label = tk.Label(model_type_frame, text='Model:', font=('Arial', 14))
 model_label.pack(side=tk.LEFT, pady=(10, 5), anchor='w')
 
-model_type_box = ttk.Combobox(model_type_frame, values=['Logistic Regression', 'K-Nearest Neighbors', 'Decision Tree', 'Support Vector Machine'], font=('Arial', 12), width=20)
+model_type_box = ttk.Combobox(model_type_frame, values=MODEL_TYPE_VALUES, font=('Arial', 12), width=20)
 model_type_box.pack(side=tk.LEFT, pady=5, padx=10)
 
 button = Button(root, text='OK', font=('Arial', 12), command=show_prediction, width=8)
